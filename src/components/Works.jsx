77e2a0975574a9ab3561fe88a71697fd17e34e01
Sophie Kim <alexsophiekim@gmail.com>
2023-11-27 12:58:47 +1300

    add some content

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 // import { data } from '../constants';
-import uxpip from '../assets/uxpip.jpg';
+import subproductux from '../assets/subproductux.jpg';
 import modalpip from '../assets/modalpip.mov';
 import extendablepip from '../assets/extendablepip.mov';
 
-import pipcover from '../assets/pipcover.jpg';
+import subproduct from '../assets/sub-product.jpg';
 
 const Works = () => {
   return (
     <div className='w-full bg-[#232931] text-white lg:p-[4rem] md:p-[2rem] sm:p-[1rem]' id='works'>
       <h1 className='text-center text-4xl py-[2rem] uppercase font-bold text-[#00df9a]'>Latest UX case study</h1>
-      <img src={pipcover} alt="PIP-UX" className='lg:px-[10rem] md:px-[4rem] sm:px-[1rem]'/>
-       <img src={uxpip} alt="PIP-UX" className='lg:px-[10rem] h-100 md:px-[4rem] sm:px-[1rem]'/>
+      <img src={subproduct} alt="sub-product-UX" className='lg:px-[10rem] md:px-[4rem] sm:px-[1rem]'/>
+       <img src={subproductux} alt="PIP-UX" className='lg:px-[10rem] h-100 md:px-[4rem] sm:px-[1rem]'/>
       <h3 className='text-center mt-5'>A/B text</h3>
        <div  className='flex flex-col justify-center items-center gap-10 py-2 lg:px-[10rem] md:px-[4rem] sm:px-[1rem] md:flex-row'>
           <video controls autoPlay loop muted width="250" height="100%">
